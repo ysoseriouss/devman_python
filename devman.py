2da303b68e1a04603072b0854ff559a553f3224e
ysoseriouss <55956428+ysoseriouss@users.noreply.github.com>
2022-12-20 16:21:06 +0300

    Add files via upload

--- devman.py
+++ devman.py
@@ -1,18 +1,19 @@
 import requests
 
-url_1 = 'https://wttr.in/Череповец?qnmTM&lang=ru'
-url_2 = 'https://wttr.in/Лондон?qnmTM&lang=ru'
-url_3 = 'https://wttr.in/SVO?qnmTM&lang=ru'
+url_template = 'http://wttr.in/{}'
+places = ['Череповец', 'Лондон', 'SVO']
+payload = {
+    'q': '',
+    'n': '',
+    'm': '',
+    'T': '',
+    'M': '',
+    'lang': 'ru'
+}
 
-response_1 = requests.get(url_1)
-response_1.raise_for_status()
 
-response_2 = requests.get(url_2)
-response_2.raise_for_status()
-
-response_3 = requests.get(url_3)
-response_3.raise_for_status()
-
-print(response_1.text)
-print(response_2.text)
-print(response_3.text)
+for place in places:
+    url = url_template.format(place)
+    responce = requests.get(url, params=payload)
+    responce.raise_for_status()
+    print(responce.text)
